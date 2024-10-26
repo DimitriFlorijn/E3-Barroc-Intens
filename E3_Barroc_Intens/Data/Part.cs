be708fd6f44078a57506616cd547dbf0f9990122
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-26 18:42:47 +0200

    Part class geüpdatet
    beta versie 1.8: code om de structuur van de tabel Parts in de database

--- E3_Barroc_Intens/Data/Part.cs
+++ E3_Barroc_Intens/Data/Part.cs
@@ -8,5 +8,11 @@ namespace E3_Barroc_Intens.Data
 {
     internal class Part
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
     }
 }
