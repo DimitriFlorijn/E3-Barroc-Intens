7ba89e373f44324816f66109b8eaab08dcefe204
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-26 18:37:08 +0200

    Customer class geüpdatet
    beta versie 1.4: Customer class code toegevoegd voor structuur tabel Customers in database

--- E3_Barroc_Intens/Data/Costumer.cs
+++ E3_Barroc_Intens/Data/Costumer.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace E3_Barroc_Intens.Data
 {
-    internal class Costumer
+    internal class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string number { get; set; }
+        public List<CustomerOder> Orders { get; set; } = new List<CustomerOder>();
+        public List<Contract> contracts { get; set; } = new List<Contract>();
     }
 }
