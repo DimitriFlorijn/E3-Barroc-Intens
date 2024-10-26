23e0c90adc1ab9d044c11a1dd3e6e0fe357df554
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-26 18:45:30 +0200

    Product class geüpdatet
    beta versie 2.0: code voor de structuur van de Products tabel in de database

--- E3_Barroc_Intens/Data/Product.cs
+++ E3_Barroc_Intens/Data/Product.cs
@@ -8,5 +8,14 @@ namespace E3_Barroc_Intens.Data
 {
     internal class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public List<Contract> contracts { get; set; } = new List<Contract>();
+        public List<CustomerOder> costumers { get; set; } = new List<CustomerOder>();
     }
 }
