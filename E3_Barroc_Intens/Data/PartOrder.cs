9eebb255aa275812c981d88a1de8ea763d174699
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-26 18:43:51 +0200

    PartOrder class geüpdatet
    beta versie 1.9: code voor de structuur van de tabel PartOder in de database

--- E3_Barroc_Intens/Data/PartOrder.cs
+++ E3_Barroc_Intens/Data/PartOrder.cs
@@ -8,5 +8,10 @@ namespace E3_Barroc_Intens.Data
 {
     internal class PartOrder
     {
+        public int PartId { get; set; }
+        public Part Part { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
