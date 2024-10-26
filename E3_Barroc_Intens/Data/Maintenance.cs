152f1be157ad1e1e85674342df3de7b4fec43f29
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-26 18:41:54 +0200

    Maintenance class geüpdatet
    beta versie 1.7: code voor de structuur van de tabel Maintenances in de database

--- E3_Barroc_Intens/Data/Maintenance.cs
+++ E3_Barroc_Intens/Data/Maintenance.cs
@@ -8,5 +8,11 @@ namespace E3_Barroc_Intens.Data
 {
     internal class Maintenance
     {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int CostumerId { get; set; }
+        public Customer Costumer { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
