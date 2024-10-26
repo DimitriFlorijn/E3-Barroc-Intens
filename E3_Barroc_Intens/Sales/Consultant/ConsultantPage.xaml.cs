6f6faf557aab34fa264a3e54abf7d954439c40f9
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-26 09:26:41 +0200

    naam wijziging ConsultantPage
    Alpha versie 0.5: naam wijziging ConsultantPage

--- E3_Barroc_Intens/Sales/Consultant/ConsultantPage.xaml.cs
+++ E3_Barroc_Intens/Sales/Consultant/ConsultantPage.xaml.cs
@@ -21,9 +21,9 @@ namespace E3_Barroc_Intens.Sales.Consultant
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ConsultantPage1 : Page
+    public sealed partial class ConsultantPage : Page
     {
-        public ConsultantPage1()
+        public ConsultantPage()
         {
             this.InitializeComponent();
         }
