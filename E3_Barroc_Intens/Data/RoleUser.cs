a31d5cfbc8d81338d2412af2a8a61af6c3e832df
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-26 18:49:02 +0200

    RoleUser class geüpdatet
    beta versie 2.2: code om de structuur van de tabel RoleUsers in de database

--- E3_Barroc_Intens/Data/RoleUser.cs
+++ E3_Barroc_Intens/Data/RoleUser.cs
@@ -8,5 +8,9 @@ namespace E3_Barroc_Intens.Data
 {
     internal class RoleUser
     {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
     }
 }
