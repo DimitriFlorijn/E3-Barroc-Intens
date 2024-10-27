e48aabffc6abf64aa2ec22d13af2b2786ff58bf6
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 21:02:45 +0100

    Invoice class toegevoegd + code toegevoegd
    beta versie 8.3: Invoice class aangemaakt en code toegevoegd

--- E3_Barroc_Intens/Data/Invoice.cs
+++ E3_Barroc_Intens/Data/Invoice.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace E3_Barroc_Intens.Data
 {
-    class Invoice
+    internal class Invoice
     {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int ContractId { get; set; }
+        public Contract Contract { get; set; }
+        public DateTime DateIssued { get; set; }
+        public DateTime DueDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public bool IsPaid { get; set; }
     }
 }
