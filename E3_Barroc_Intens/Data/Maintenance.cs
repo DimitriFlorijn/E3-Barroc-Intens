4c79fc52c2abe6ae42fca2d7624f447ed66d7710
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 21:01:04 +0100

    d en naamsverandering velden class Maintenance
    beta versie 8.2: Id veld aangemaakt en naam veranderd van DateTime naar AppointmentDate in Maintenance class

--- E3_Barroc_Intens/Data/Maintenance.cs
+++ E3_Barroc_Intens/Data/Maintenance.cs
@@ -8,11 +8,12 @@ namespace E3_Barroc_Intens.Data
 {
     internal class Maintenance
     {
+        public int Id { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
         public int CostumerId { get; set; }
         public Customer Costumer { get; set; }
         public string Description { get; set; }
-        public DateTime DateTime { get; set; }
+        public DateTime AppointmentDate { get; set; }
     }
 }
