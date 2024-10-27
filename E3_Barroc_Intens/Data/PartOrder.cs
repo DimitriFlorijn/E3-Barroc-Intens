5b5cdf5d1f269e55e1af3b4cb7250aa9946d2412
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 20:59:55 +0100

    Id en naamsverandering velden class PartOrder
    beta versie 8.1: Id veld aangemaakt en naam veranderd van DateTime naar OrderDate in PartOrder class

--- E3_Barroc_Intens/Data/PartOrder.cs
+++ E3_Barroc_Intens/Data/PartOrder.cs
@@ -8,10 +8,11 @@ namespace E3_Barroc_Intens.Data
 {
     internal class PartOrder
     {
+        public int Id { get; set; }
         public int PartId { get; set; }
         public Part Part { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
-        public DateTime DateTime { get; set; }
+        public DateTime OrderDate { get; set; }
     }
 }
