6a7c74812a3ae374e4d605d58bcb2906d89d492a
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 12:46:56 +0100

    methode OnNatigatedTo in LopigPage.cs
    beta versie 5.4: methode OnNavigated toegevoegd in de LogInPage.cs

--- E3_Barroc_Intens/Auth/LogInPage.xaml.cs
+++ E3_Barroc_Intens/Auth/LogInPage.xaml.cs
@@ -27,5 +27,10 @@ namespace E3_Barroc_Intens.Auth
         {
             this.InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
     }
 }
