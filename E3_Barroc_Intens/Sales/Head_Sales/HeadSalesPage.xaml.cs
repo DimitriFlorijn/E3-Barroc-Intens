d12dd41278b1e0142f082e8a37ef67221c8c7e09
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 13:36:27 +0100

    methode OnNavigatedTo toegevoegd in HeadSalesPage.cs
    beta versie 5.8: methode OnNavigatedTo toegevoegd in HeadSalesPage

--- E3_Barroc_Intens/Sales/Head_Sales/HeadSalesPage.xaml.cs
+++ E3_Barroc_Intens/Sales/Head_Sales/HeadSalesPage.xaml.cs
@@ -27,5 +27,10 @@ namespace E3_Barroc_Intens.Sales.Head_Sales
         {
             this.InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
     }
 }
