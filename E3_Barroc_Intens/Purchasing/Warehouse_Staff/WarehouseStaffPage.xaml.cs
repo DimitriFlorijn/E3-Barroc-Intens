db0327e535793ce89a1d2c3ed4fd9fd331d434bb
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 13:39:55 +0100

    methode OnNavigatedTo toegevoegd in WarehouseStaffPage.cs
    beta versie 6.0: methode OnNavigatedTo toegevoegd in WarehouseStaffPage.cs

--- E3_Barroc_Intens/Purchasing/Warehouse_Staff/WarehouseStaffPage.xaml.cs
+++ E3_Barroc_Intens/Purchasing/Warehouse_Staff/WarehouseStaffPage.xaml.cs
@@ -27,5 +27,10 @@ namespace E3_Barroc_Intens.Purchasing.Warehouse_Staff
         {
             this.InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
     }
 }
