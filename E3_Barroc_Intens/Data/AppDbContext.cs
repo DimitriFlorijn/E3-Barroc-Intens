07f65735b523bf675f74c7d37451acb92017cbe8
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 22:26:40 +0100

    twee extra tabellen toegevoegd + testdata
    beta versie 9.2: tabellen IncendentReport en Invoices toegevoegd + testdata voor de tabellen Brands, Products, Bean en Customers

--- E3_Barroc_Intens/Data/AppDbContext.cs
+++ E3_Barroc_Intens/Data/AppDbContext.cs
@@ -24,6 +24,8 @@ namespace E3_Barroc_Intens.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<RoleUser> RoleUsers { get; set; }
         public DbSet<Storage> Storages { get; set; }
+        public DbSet<IncendentReport> IncendentReports { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -299,6 +301,105 @@ namespace E3_Barroc_Intens.Data
                     RoleId = 11,
                     UserId = 15,
                 }
+            );
+
+            modelBuilder.Entity<Brand>().HasData(
+                new Brand
+                {
+                    Id = 1,
+                    Name = "Barroc Intens"
+                }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Barroc Intens Italian Light",
+                    Description = "S234FREKT",
+                    Type = "Koffieautomaat",
+                    Price = 499.00M,
+                    InstallationCost = 289.00M,
+                    BrandId = 1,
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Barroc Intens Italian",
+                    Description = "S234KNDPF",
+                    Type = "Koffieautomaat",
+                    Price = 599.00M,
+                    InstallationCost = 289.00M,
+                    BrandId = 1,
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Barroc Intens Italian Deluxe",
+                    Description = "S234NNBMV",
+                    Type = "Koffieautomaat",
+                    Price = 799.00M,
+                    InstallationCost = 375.00M,
+                    BrandId = 1,
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "Barroc Intens Italian Deluxe Special",
+                    Description = "S234MMPLA",
+                    Type = "Koffieautomaat",
+                    Price = 999.00M,
+                    InstallationCost = 375.00M,
+                    BrandId = 1,
+                }
+            );
+
+            modelBuilder.Entity<Bean>().HasData(
+                new Bean
+                {
+                    Id = 1,
+                    Name = "Espresso Beneficio",
+                    Description = "Een toegankelijke en zachte koffie. Hij is afkomstig van de Finca El Limoncillo, een weelderige plantage aan de rand van het regenwoud in de Matagalpa regio in Nicaragua.",
+                    Type = "S239KLIUP",
+                    PricePerKg = 21.60m
+                },
+                new Bean
+                {
+                    Id = 2,
+                    Name = "Yellow Bourbon Brasil",
+                    Description = "Koffie van de oorspronkelijke koffiestruik (de Bourbon) met gele koffiebessen. Deze zeldzame koffie heeft de afgelopen 20 jaar steeds meer erkenning gekregen en vele prijzen gewonnen.",
+                    Type = "S239MNKLL",
+                    PricePerKg = 23.20m
+                },
+                new Bean
+                {
+                    Id = 3,
+                    Name = "Espresso Roma",
+                    Description = "Een Italiaanse espresso met een krachtig karakter en een aromatische afdronk.",
+                    Type = "S239IPPSD",
+                    PricePerKg = 20.80m
+                },
+                new Bean
+                {
+                    Id = 4,
+                    Name = "Red Honey Honduras",
+                    Description = "De koffie is geproduceerd volgens de honey-methode. Hierbij wordt de koffieboon in haar vruchtvlees gedroogd, waardoor de zoete fruitsmaak diep in de boon trekt. Dit levert een éxtra zoete koffie op.",
+                    Type = "S239EVVFS",
+                    PricePerKg = 27.80m
+                }
+            );
+
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer { Id = 1, Name = "Klant Actief 1", Email = "actief1@example.com", Location = "Locatie 1", Number = "0612345678", BkrRegistered = false },
+                new Customer { Id = 2, Name = "Klant Actief 2", Email = "actief2@example.com", Location = "Locatie 2", Number = "0612345679", BkrRegistered = false },
+                new Customer { Id = 3, Name = "Klant Actief 3", Email = "actief3@example.com", Location = "Locatie 3", Number = "0612345680", BkrRegistered = false },
+                new Customer { Id = 4, Name = "Klant Actief 4", Email = "actief4@example.com", Location = "Locatie 4", Number = "0612345681", BkrRegistered = false },
+                new Customer { Id = 5, Name = "Klant Actief 5", Email = "actief5@example.com", Location = "Locatie 5", Number = "0612345682", BkrRegistered = false },
+                new Customer { Id = 151, Name = "Klant Inactief 1", Email = "inactief1@example.com", Location = "Locatie 151", Number = "0612345688", BkrRegistered = false },
+                new Customer { Id = 152, Name = "Klant Inactief 2", Email = "inactief2@example.com", Location = "Locatie 152", Number = "0612345689", BkrRegistered = true },
+                new Customer { Id = 153, Name = "Klant Inactief 3", Email = "inactief3@example.com", Location = "Locatie 153", Number = "0612345690", BkrRegistered = false },
+                new Customer { Id = 154, Name = "Klant Inactief 4", Email = "inactief4@example.com", Location = "Locatie 154", Number = "0612345691", BkrRegistered = false },
+                new Customer { Id = 155, Name = "Klant Inactief 5", Email = "inactief5@example.com", Location = "Locatie 155", Number = "0612345692", BkrRegistered = false }
             );
         }
     }
