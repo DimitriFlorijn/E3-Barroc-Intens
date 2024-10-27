44e0a89ab7c0246d9d1a8c800340cafdc9d90a78
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 13:38:00 +0100

    methode OnNavigatedTo toegevoegd in ConsultantPage.cs
    beta versie 5.9: methode OnNivigatedTo toegevoegd in ConsultantPage.cs

--- E3_Barroc_Intens/Sales/Consultant/ConsultantPage.xaml.cs
+++ E3_Barroc_Intens/Sales/Consultant/ConsultantPage.xaml.cs
@@ -27,5 +27,10 @@ namespace E3_Barroc_Intens.Sales.Consultant
         {
             this.InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
     }
 }
