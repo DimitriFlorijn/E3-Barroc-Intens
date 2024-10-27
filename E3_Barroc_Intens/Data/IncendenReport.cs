f4072e8a039ab33f2b3d205380a4d7095c67e68b
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 22:19:46 +0100

    class IncendentReport toegevoegd
    beta versie 9.0: IncendentReport class aangemaakt en gevult met code voor de tabel structuur

---
+++ E3_Barroc_Intens/Data/IncendenReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_Barroc_Intens.Data
+{
+    class IncendentReport
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime DateReported { get; set; }
+        public string InitialMessage { get; set; }
+        public string CoffeeMachineType { get; set; }
+        public string FaultCode { get; set; }
+        public bool IsResolved { get; set; }
+        public string? FollowUp { get; set; }
+        public string IncidentDescription { get; set; }
+        public List<Part> UsedParts { get; set; }
+    }
+}
