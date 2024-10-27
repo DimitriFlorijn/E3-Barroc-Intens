2ec8ad6f5ac915d175905f86a311cb9e979dbaad
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-27 13:45:37 +0100

    methode OnNavigatedTo toegevoegd in HeadMaintenancePage.cs
    beta versie 6.5: methode OnNavigatedTo toegevoegd in HeadMaintenancePage.cs

--- E3_Barroc_Intens/Maintenance/Head_Maintenance/HeadMaintenancePage.xaml.cs
+++ E3_Barroc_Intens/Maintenance/Head_Maintenance/HeadMaintenancePage.xaml.cs
@@ -27,5 +27,10 @@ namespace E3_Barroc_Intens.Maintenance.Head_Maintenance
         {
             this.InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
     }
 }
