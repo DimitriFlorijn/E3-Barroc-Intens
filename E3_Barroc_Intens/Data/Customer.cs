15df0e419dae9fcb4201c5a82fc9c8ca4697c3f2
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-10-28 12:13:24 +0100

    Vernieuwd bestand Customer
    hernoemt bestand van oud bestand Costumer vernoemt naar Customer

---
+++ E3_Barroc_Intens/Data/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_Barroc_Intens.Data
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string Number { get; set; }
+        public bool BkrRegistered { get; set; }
+        public List<CustomerOder> Orders { get; set; } = new List<CustomerOder>();
+        public List<Contract> contracts { get; set; } = new List<Contract>();
+    }
+}
